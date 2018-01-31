612232ea060cc40b7c85e7cf9feee5fdefbdf8f3
hp <namstudionsk@gmail.com>
2018-01-31 23:00:56 +0700

    Fix Exception

--- src/x42/Sicario.java
+++ src/x42/Sicario.java
@@ -18,12 +18,20 @@ public class Sicario {
             int i = 10;
 
             i /= 0;
+
+            int c[] = {42};
+
+            c[42] = 99;
         }
-/*        catch (Exception e){
-            log.error("Catch");
-        }*/
+        catch (ArrayIndexOutOfBoundsException e){
+            log.error("Array fuck", e);
+        }
         catch (RuntimeException e2){
-            log.error("Catch");
+            log.error("Catch2");
+            log.error("1", e2);
+            //e2.printStackTrace();
+            log.error("Catch22");
+            log.error("~" + e2 + "~");
         }
         finally {
             log.debug("Done");
