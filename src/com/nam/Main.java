1bb8f1e651ca31e0cd65af0c5cffed98752a2e34
hp <namstudionsk@gmail.com>
2018-01-31 23:11:21 +0700

    Fix Exception

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-	// write your code here
 
         log.info("Begin");
 
